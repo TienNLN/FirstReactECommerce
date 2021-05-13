90e14e58eb03677e8e91b7f1d48565da4d30d9a5
Nguyen Lam Nhat Tien <tien20001810@gmail.com>
2021-05-13 13:21:27 +0700

    Configure MainContainItems component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { Component } from "react";
+import MainBody from "./components/body-component/MainBody";
+import Footer from "./components/footer-components/Footer";
 import Header from "./components/header-components/Header";
 
 class App extends Component {
     render() {
         return (
-            <Header/>
+            <div>
+                <Header/>
+                <MainBody />
+                <Footer/>
+            </div>
         );
     }
 }
---
+++ src/components/footer-components/Footer.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import "./Footer.css";
+
+class Footer extends Component {
+    render () {
+        return(
+            <div className="text-center p-3 bg-dark text-light footer">
+                Copyright@2021 by Cris
+            </div>
+        );
+    }
+}
+
+export default Footer;
